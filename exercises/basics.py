9a3ab68a133f74600e9d4d9d9c0da91cdde96aaa
kaybee <passionno1@qq.com>
2023-05-06 09:59:36 +0800

    add basic exercise

---
+++ exercises/basics.py
@@ -0,0 +1,9 @@
+address = ["Friend Company", "18", "dd"]
+pins = {"kay": 1111, "liu": 2222, "Jack": 3333}
+
+print(address[0],address[1])
+
+pin = int(input("Enter your pin: "))
+
+def find_in_file(f):
+    open("sample.txt")
